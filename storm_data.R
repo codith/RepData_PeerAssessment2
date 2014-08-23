3fc75f325053a48e66c63e172349552fb4b76f76
codith <marcus.carr@gmail.com>
2014-08-23 15:47:35 -0700

    coding mostly done

--- storm_data.R
+++ storm_data.R
@@ -1,6 +1,5 @@
 setwd("~/coursera/repdata/RepData_PeerAssessment2")
 
-
 if(!(exists("storm_data"))) {
         # Set up variables for temp file.
         fileURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
@@ -14,4 +13,13 @@ if(!(exists("storm_data"))) {
         
         # Read in the data and close the file.
         storm_data <- read.csv(fh)
+        
+        # Also download some documentation.
+        doc1URL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2Fpd01016005curr.pdf"
+        doc1name <- "repdata%2Fpeer2_doc%2Fpd01016005curr.pdf"
+        download.file(doc1URL, destfile = doc1name, method = "curl")
+        
+        faqURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf"
+        faqname <- "repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf"
+        download.file(faqURL, destfile = faqname, method = "curl")
 }
